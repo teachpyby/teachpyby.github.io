d0f62f524499016e7d3ee6378dc371d6573e07de
baydet <evsyuchenya@targetprocess.com>
2020-12-22 22:30:39 +0300

    add lesson 2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t teachpyby.github.io:latest .
-docker run -v "$(pwd):/var/www/data" -p80:80 teachpyby.github.io:latest
+docker run -v "$(pwd):/var/www/data" -p1080:1080 teachpyby.github.io:latest
